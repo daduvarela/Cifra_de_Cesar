e8f568b0087928ccb1f4ef613416bdf5f478edf5
Maria Eduarda varela <madu.varellah@gmail.com>
2022-10-22 01:16:45 -0300

    Projeto Individual

---
+++ base64.js
@@ -0,0 +1,9 @@
+let encode = document.querySelector('#encode');
+let decode = document.querySelector('#decode');
+let output = document.querySelector('textarea');
+encode.addEventListener('click', () =>{
+   output.value = btoa(output.value) ;
+});
+decode.addEventListener('click', () =>{
+    output.value = atob(output.value) ;
+});
